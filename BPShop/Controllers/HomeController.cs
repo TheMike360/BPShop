a2501c9accd83df7e1cb1bd2f7609a5a66a9198b
NikitaZol <dr.bread90103@gmail.com>
2023-11-26 21:38:19 +0600

    Добавил функционал на кнопки "Продукты" и "Добавить продукты"

--- BPShop/Controllers/HomeController.cs
+++ BPShop/Controllers/HomeController.cs
@@ -11,177 +11,188 @@ using System.Web.Mvc;
 
 namespace BPShop.Controllers
 {
-	public class HomeController : Controller
-	{
-		private readonly MYContext context;
+    public class HomeController : Controller
+    {
+        private readonly MYContext context;
 
-		public HomeController() : this(new MYContext())
-		{
-		}
+        public HomeController() : this(new MYContext())
+        {
+        }
 
-		public HomeController(MYContext context)
-		{
-			this.context = context;
-		}
+        public HomeController(MYContext context)
+        {
+            this.context = context;
+        }
 
-		public async Task<ActionResult> Index()
-		{
-			IQueryable<Product> products = context.Products.OrderByDescending(x => x.ID);
-			//для range slider цены
-			ViewBag.MaxCost = (int)products.Select(x => x.Cost).Max();
-			ViewBag.MinCost = (int)products.Select(x => x.Cost).Min();
+        public async Task<ActionResult> Index()
+        {
+            IQueryable<Product> products = context.Products.OrderByDescending(x => x.ID);
+            //для range slider цены
+            ViewBag.MaxCost = (int)products.Select(x => x.Cost).Max();
+            ViewBag.MinCost = (int)products.Select(x => x.Cost).Min();
 
-			List<Product> result = await products.Take(10).ToListAsync();
-			return View(result);
-		}
+            List<Product> result = await products.Take(10).ToListAsync();
+            return View(result);
+        }
 
-		public async Task<ActionResult> Products(decimal maxRange = 0, decimal minRange = 0, SortType sortType = SortType.defaultSort, string search = "")
-		{
-			IQueryable<Product> products = context.Products.OrderByDescending(x => x.ID);
+        public async Task<ActionResult> Products(decimal maxRange = 0, decimal minRange = 0, SortType sortType = SortType.defaultSort, string search = "")
+        {
+            IQueryable<Product> products = context.Products.OrderByDescending(x => x.ID);
 
-			//для range slider цены
-			ViewBag.MaxCost = (int)products.Select(x => x.Cost).Max();
-			ViewBag.MinCost = (int)products.Select(x => x.Cost).Min();
+            //для range slider цены
+            ViewBag.MaxCost = (int)products.Select(x => x.Cost).Max();
+            ViewBag.MinCost = (int)products.Select(x => x.Cost).Min();
 
-			ViewBag.MaxRange = ViewBag.MaxCost;
-			ViewBag.MinRange = ViewBag.MinCost;
+            ViewBag.MaxRange = ViewBag.MaxCost;
+            ViewBag.MinRange = ViewBag.MinCost;
 
-			if (minRange != 0 && maxRange != 0)
-			{
-				ViewBag.MaxRange = maxRange;
-				ViewBag.MinRange = minRange;
-				products = products.Where(x => x.Cost >= minRange && x.Cost <= maxRange);
-			}
-			if (!string.IsNullOrEmpty(search))
-			{
-				ViewBag.Search = search;
-				products = products.Where(x => x.SearhPrompt.Contains(search));
-			}
-			if (sortType != SortType.defaultSort)
-			{
-				switch (sortType)
-				{
-					case SortType.oldProducts:
-						products = products.OrderBy(x => x.ID);
-						break;
-					case SortType.newProducts:
-						products = products.OrderByDescending(x => x.ID);
-						break;
-					case SortType.costDesc:
-						products = products.OrderByDescending(x => x.Cost);
-						break;
-					case SortType.costAsc:
-						products = products.OrderBy(x => x.Cost);
-						break;
-				}
-			}
+            if (minRange != 0 && maxRange != 0)
+            {
+                ViewBag.MaxRange = maxRange;
+                ViewBag.MinRange = minRange;
+                products = products.Where(x => x.Cost >= minRange && x.Cost <= maxRange);
+            }
+            if (!string.IsNullOrEmpty(search))
+            {
+                ViewBag.Search = search;
+                products = products.Where(x => x.SearhPrompt.Contains(search));
+            }
+            if (sortType != SortType.defaultSort)
+            {
+                switch (sortType)
+                {
+                    case SortType.oldProducts:
+                        products = products.OrderBy(x => x.ID);
+                        break;
+                    case SortType.newProducts:
+                        products = products.OrderByDescending(x => x.ID);
+                        break;
+                    case SortType.costDesc:
+                        products = products.OrderByDescending(x => x.Cost);
+                        break;
+                    case SortType.costAsc:
+                        products = products.OrderBy(x => x.Cost);
+                        break;
+                }
+            }
 
-			//вид сортировки
-			ViewBag.SortType = sortType;
-			var cart = GetCart();
+            //вид сортировки
+            ViewBag.SortType = sortType;
+            var cart = GetCart();
 
-			ViewBag.IsHaveCart = cart.Count() > 0;
-			ViewBag.CartCount = countCartItems(cart);
+            ViewBag.IsHaveCart = cart.Count() > 0;
+            ViewBag.CartCount = countCartItems(cart);
 
 
-			List<Product> result = await products.ToListAsync();
-			return View(result);
-		}
+            List<Product> result = await products.ToListAsync();
+            return View(result);
+        }
 
-		public async Task<ActionResult> ShowProduct(int Id)
-		{
-			Product Item = await context.Products.FirstOrDefaultAsync(x => x.ID == Id);
-			return View(Item);
-		}
+        public async Task<ActionResult> ShowProduct(int Id)
+        {
+            Product Item = await context.Products.FirstOrDefaultAsync(x => x.ID == Id);
+            return View(Item);
+        }
 
-		public async Task<ActionResult> Cart()
-		{
-			List<CartModel> Cart = GetCart();
-			List<int> addedCartPoructIds = Cart.Select(x => x.ProductId).ToList();
-			ViewBag.CartPrdouctCounter = Cart;
+        public async Task<ActionResult> Cart()
+        {
+            List<CartModel> Cart = GetCart();
+            List<int> addedCartPoructIds = Cart.Select(x => x.ProductId).ToList();
+            ViewBag.CartPrdouctCounter = Cart;
 
-			return View(await context.Products.Where(x => addedCartPoructIds.Contains(x.ID)).ToListAsync());
-		}
+            return View(await context.Products.Where(x => addedCartPoructIds.Contains(x.ID)).ToListAsync());
+        }
 
-		public int AddToCart(int productId, int quantity = 1)
-		{
-			List<CartModel> cart = GetCart();
-			if (countCartItems(cart) > 100)
-			{
-				return -100;
-			}
+        public int AddToCart(int productId, int quantity = 1)
+        {
+            List<CartModel> cart = GetCart();
+            if (countCartItems(cart) > 100)
+            {
+                return -100;
+            }
 
-			CartModel existingItem = cart.FirstOrDefault(item => item.ProductId == productId);
+            CartModel existingItem = cart.FirstOrDefault(item => item.ProductId == productId);
 
-			if (existingItem != null)
-			{
-				existingItem.Quantity += quantity;
-			}
-			else
-			{
-				cart.Add(new CartModel { ProductId = productId, Quantity = quantity });
-			}
+            if (existingItem != null)
+            {
+                existingItem.Quantity += quantity;
+            }
+            else
+            {
+                cart.Add(new CartModel { ProductId = productId, Quantity = quantity });
+            }
 
-			return 0;
-		}
+            return 0;
+        }
 
-		[HttpPost]
-		public async Task<int> ConfirmOrder(Order order)
-		{
-			if (order.Phone.Contains("_"))
-			{
-				return -1;
-			}
+        [HttpPost]
+        public async Task<int> ConfirmOrder(Order order)
+        {
+            if (order.Phone.Contains("_"))
+            {
+                return -1;
+            }
 
-			order.Time = DateTime.Now;
+            order.Time = DateTime.Now;
 
-			context.Orders.Add(order);
-			await context.SaveChangesAsync();
+            context.Orders.Add(order);
+            await context.SaveChangesAsync();
 
-			return 0;
-		}
+            return 0;
+        }
 
-		public ActionResult About()
-		{
-			return View();
-		}
+        public ActionResult About()
+        {
+            return View();
+        }
 
-		public ActionResult Contact()
-		{
-			return View();
-		}
+        public ActionResult Contact()
+        {
+            return View();
+        }
 
-		private List<CartModel> GetCart()
-		{
-			var cart = Session["Cart"] as List<CartModel>;
-			if (cart == null)
-			{
-				cart = new List<CartModel>();
-				Session["Cart"] = cart;
-			}
-			return cart;
-		}
+        private List<CartModel> GetCart()
+        {
+            var cart = Session["Cart"] as List<CartModel>;
+            if (cart == null)
+            {
+                cart = new List<CartModel>();
+                Session["Cart"] = cart;
+            }
+            return cart;
+        }
 
-		public ActionResult AdminPanel()
-		{
-			return View();
-		}
+        public ActionResult AdminPanel()
+        {
+            return View();
+        }
 
-		[HttpPost]
-		public async Task AdminPanelAdd(Product product)
-		{
-			context.Products.Add(product);
-			await context.SaveChangesAsync();
-		}
+        [HttpPost]
+        public async Task AdminPanelAdd(Product product)
+        {
+            context.Products.Add(product);
+            await context.SaveChangesAsync();
+        }
 
-		private int countCartItems(List<CartModel> cart)
-		{
-			int cartCount = 0;
-			foreach (var item in cart)
-			{
-				cartCount += item.Quantity;
-			}
-			return cartCount;
-		}
-	}
+        public ActionResult AddProductsForm()
+        {
+            return View();
+        }
+
+        public ActionResult GetProductsTable()
+        {
+            IQueryable<Product> Products = context.Products.OrderByDescending(x => x.ID);
+            return View(Products);
+        }
+
+        private int countCartItems(List<CartModel> cart)
+        {
+            int cartCount = 0;
+            foreach (var item in cart)
+            {
+                cartCount += item.Quantity;
+            }
+            return cartCount;
+        }
+    }
 }

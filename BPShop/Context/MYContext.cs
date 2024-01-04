6399aba824a0112e335d6990794e4c91847dfe9b
TheMike360 <artemfedorov239@gmail.com>
2024-01-04 22:08:28 +0600

    crush mySQl

--- BPShop/Context/MYContext.cs
+++ BPShop/Context/MYContext.cs
@@ -8,13 +8,12 @@ namespace BPShop.Context
 		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 		{
 #if DEBUG
-			//string connectionString = "server=localhost;user=root;password=;database=BPDataBase;";
-			string connectionString = "Server=srv-wpleskdb01.ps.kz:3306;Database=bpflower_db;User Id=bpflower_user;Password=20S2@d01Dude;";
+			string connectionString = "server=localhost;user=root;password=;database=BPDataBase;";
 #else
-            string connectionString = "Server=srv-wpleskdb01.ps.kz;Port=3309;Database=bpflower_db;User Id=bpflower_user;Password=20S2@d01Dude; ";
+            string connectionString = "Server=srv-wpleskdb01.ps.kz;Port=3306;Database=bpflower_db;User Id=bpflower_user;Password=20S2@d01Dude; ";
 #endif
 
-            optionsBuilder.UseMySQL(connectionString);
+			optionsBuilder.UseMySQL(connectionString);
 		}
 
 		public DbSet<Product> Products { get; set; }
